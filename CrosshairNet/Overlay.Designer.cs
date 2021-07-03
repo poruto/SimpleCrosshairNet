03138c9854233ab6b3be3a9ee6463fda5f067c1d
poruto <42906598+poruto@users.noreply.github.com>
2021-07-03 16:02:03 +0200

    Add files via upload

---
+++ CrosshairNet/Overlay.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace CrosshairNet
+{
+    partial class Overlay
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.Black;
+            this.btn_Close.FlatAppearance.BorderSize = 0;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.Location = new System.Drawing.Point(2, 2);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(75, 23);
+            this.btn_Close.TabIndex = 0;
+            this.btn_Close.Text = "CLOSE";
+            this.btn_Close.UseVisualStyleBackColor = false;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // Overlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Magenta;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Close);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Overlay";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "CrosshairNet";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Magenta;
+            this.Load += new System.EventHandler(this.Overlay_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Close;
+    }
+}
+
---
+++ CrosshairNet/Overlay.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CrosshairNet
+{
+    public partial class Overlay : Form
+    {
+        public Pen pen;
+        public bool crosshair = true;
+        public int thickness = 2;
+        public int height = 10;
+        public int width = 10;
+        public int gap = 0;
+        public Color color = Color.FromArgb(255, 255, 0, 0); // or Color.Red, Color.Green
+
+        public Overlay()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x80;
+                return cp;
+            }
+        }
+
+        public void update()
+        {
+            this.TopMost = true;
+            this.pen = new Pen(color, thickness);
+            Invalidate();
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            int def_gap = 2;
+            if (crosshair)
+            {
+                int x_start = Screen.PrimaryScreen.Bounds.Width / 2;
+                int y_start = Screen.PrimaryScreen.Bounds.Height / 2;
+
+                e.Graphics.DrawLine(pen, x_start - width, y_start, x_start - def_gap - gap, y_start);
+                e.Graphics.DrawLine(pen, x_start + def_gap + gap, y_start, x_start + width, y_start);
+
+                e.Graphics.DrawLine(pen, x_start, y_start - height, x_start, y_start - def_gap - gap);
+                e.Graphics.DrawLine(pen, x_start, y_start + height, x_start, y_start + def_gap + gap);
+            }
+        }
+
+        private void Overlay_Load(object sender, EventArgs e)
+        {
+            this.Left = 0;
+            this.Top = 0;
+            this.Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            btn_Close.TabStop = false;
+            update();
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
